448a039c96dff8af1a6599313a82ce8f51e1513f
kylejoseph46 <kylejosephswe@gmail.com>
2023-02-23 14:24:38 -0800

    Finalized version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useRef } from 'react'
 import './App.css'
 import DarkMode from './DarkMode'
+import blackImage from './photos/blackImage.jpg'
+import whiteImage from './photos/whiteImage.png'
 
 
 function App() {
@@ -8,7 +10,15 @@ function App() {
   const [animeImage, setanimeImage] = useState([])
   const [animeLink, setanimeLink] = useState([])
   const inputRef = useRef()
-
+  let imageToUse
+  if (localStorage.getItem("theme") === 'light')
+  {
+    imageToUse = whiteImage
+  }
+  else 
+  {
+    imageToUse = blackImage
+  }
 
   const options = {
     method: 'GET',
@@ -29,8 +39,6 @@ function App() {
 
 
         response.data.map(anime => {
-          console.log(anime.title)
-          console.log(anime.image)
           setanimeTitle((prevanimeTitle) => (
             [...prevanimeTitle, anime.title]
           ))
@@ -55,7 +63,7 @@ function App() {
 
 
   return (
-    <>
+    <div className="App">
         <nav>
         <h1 className="titler">Anime Retriever</h1>
         <DarkMode />
@@ -69,78 +77,102 @@ function App() {
       <div className="anime">
         <div className="anime-card">
           <a href={animeLink[0]}>
-            {<img src={animeImage[0]} alt="img1"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[0]} alt="img1"></img>}
           </a>
+          <a href={animeLink[0]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[0]}</h6>
+          </a>
         </div>
         <div className="anime-card">
           <a href={animeLink[1]}>
-            {<img src={animeImage[1]} alt="img2"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[1]} alt="img1"></img>}
           </a>
+          <a href={animeLink[1]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[1]}</h6>
+          </a>
         </div>
         <div className="anime-card">
           <a href={animeLink[2]}>
-            {<img src={animeImage[2]} alt="img3"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[2]} alt="img1"></img>}
           </a>
+          <a href={animeLink[2]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[2]}</h6>
+          </a>
         </div>
         <div className="anime-card">
           <a href={animeLink[3]}>
-            {<img src={animeImage[3]} alt="img4"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[3]} alt="img1"></img>}
           </a>
+          <a href={animeLink[3]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[3]}</h6>
+          </a>
         </div>
         <div className="anime-card">
           <a href={animeLink[4]}>
-            {<img src={animeImage[4]} alt="img5"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[4]} alt="img1"></img>}
           </a>
+          <a href={animeLink[4]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[4]}</h6>
+          </a>
         </div>
         <div className="anime-card">
           <a href={animeLink[5]}>
-            {<img src={animeImage[5]} alt="img6"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[5]} alt="img1"></img>}
           </a>
+          <a href={animeLink[5]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[5]}</h6>
+          </a>
         </div>
         <div className="anime-card">
           <a href={animeLink[6]}>
-            {<img src={animeImage[6]} alt="img7"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[6]} alt="img1"></img>}
           </a>
+          <a href={animeLink[6]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[6]}</h6>
+          </a>
         </div>
         <div className="anime-card">
           <a href={animeLink[7]}>
-            {<img src={animeImage[7]} alt="img8"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[7]} alt="img1"></img>}
           </a>
+          <a href={animeLink[7]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[7]}</h6>
+          </a>
         </div>
         <div className="anime-card">
           <a href={animeLink[8]}>
-            {<img src={animeImage[8]} alt="img9"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[8]} alt="img1"></img>}
           </a>
+          <a href={animeLink[8]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[8]}</h6>
+          </a>
         </div>
         <div className="anime-card">
           <a href={animeLink[9]}>
-            {<img src={animeImage[9]} alt="img10"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[9]} alt="img1"></img>}
           </a>
+          <a href={animeLink[9]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[9]}</h6>
+          </a>
         </div>
         <div className="anime-card">
           <a href={animeLink[10]}>
-            {<img src={animeImage[10]} alt="img11"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[10]} alt="img1"></img>}
           </a>
+          <a href={animeLink[10]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[10]}</h6>
+          </a>
         </div>
         <div className="anime-card">
           <a href={animeLink[11]}>
-            {<img src={animeImage[11]} alt="img12"></img>}
+            {<img src={animeImage.length === 0 ? imageToUse : animeImage[11]} alt="img1"></img>}
           </a>
+          <a href={animeLink[11]} target="_blank" rel="noreferrer">
           <h6>{animeTitle[11]}</h6>
+          </a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
